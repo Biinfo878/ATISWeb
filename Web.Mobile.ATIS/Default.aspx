3a8a16b6b329b3867eafadbd94439d947eed3d4e
Biinfo878 <shsuccessbarname@gmail.com>
2020-05-06 21:17:18 +0330

    Add project files.

---
+++ Web.Mobile.ATIS/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Web.Mobile.ATIS._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+        <div class="col-md-4">
+            <h2>ATIS Mobile</h2>
+            <p>
+                <asp:Button ID="Button1" runat="server" Text="دانلود کنید" Font-Names="B Homa" Font-Size="Small" OnClick="Button1_Click" />
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>ATIS Desktop</h2>
+            <p>
+                <asp:Button ID="Button2" runat="server" Text="دانلود کنید" Font-Names="B Homa" Font-Size="Small" />
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>ATIS Web</h2>
+            <p>
+            </p>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Web.Mobile.ATIS/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.IO;
+using System.Web.UI.WebControls;
+
+namespace Web.Mobile.ATIS
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.IO.FileStream fs = null;
+                fs = System.IO.File.Open(HttpContext.Current.Server.MapPath("~/App_Data/ATISMobile.APK"), FileMode.Open);
+                byte[] btFile = new byte[fs.Length];
+                fs.Read(btFile, 0, Convert.ToInt32(fs.Length));
+                fs.Close();
+                Response.Buffer = true;
+                Response.Expires = 0;
+                Response.ContentType = "application/octet-stream";
+                Response.AddHeader("Content-Type", "application/octet-stream");
+                //Response.AddHeader("Content-Length", btFile.Length.ToString);
+                Response.AddHeader("Content-Disposition", "attachment;filename=ATISMobile.apk");
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.BinaryWrite(btFile);
+                Response.End();
+            }
+            catch (Exception ex)
+            { return; }
+        }
+    }
+}
